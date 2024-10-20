3faac7344d13d0c94467e841df970f96ddb59a77
Florian Lauch <florian.lauchh@gmail.com>
2024-10-20 21:04:27 +0900

    feat(components): Card component

---
+++ front/src/components/ui/card.stories.tsx
@@ -0,0 +1,59 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+
+const meta: Meta = {
+	component: Card,
+	title: "Card",
+	args: {
+		className: "",
+	},
+	argTypes: {
+		className: {
+			type: "string",
+			control: "text",
+		},
+	},
+};
+type Story = StoryObj;
+
+const Render = (args: Meta) => (
+	<Card {...args}>
+		<CardHeader>
+			<CardTitle>Card Title</CardTitle>
+			<CardDescription>Small description of the content of the card</CardDescription>
+		</CardHeader>
+		<CardContent>Card content</CardContent>
+		<CardFooter>Card footer</CardFooter>
+	</Card>
+);
+
+export const Default: Story = {
+	args: {
+		className: "",
+	},
+	parameters: {
+		backgrounds: {
+			default: "light",
+		},
+	},
+	render: Render,
+};
+
+export const DarkMode: Story = {
+	args: {
+		className: "",
+	},
+	parameters: {
+		backgrounds: {
+			default: "dark",
+		},
+	},
+	render: (args) => (
+		<div className="dark">
+			<Render {...args} />
+		</div>
+	),
+};
+
+export default meta;
