934b473f2895b089e5e5e6807271dd8d35958f65
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-10-21 05:32:03 +0900

    feat: Auth with wrapper and Privy working

---
+++ front/src/components/BedrockAccountMenu.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import { useLogout } from "@privy-io/react-auth";
+import { BadgeCheck, Bell, ChevronsUpDown, CreditCard, LogOut, Sparkles } from "lucide-react";
+import { useDisconnect } from "wagmi";
+
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { SidebarMenuButton } from "@/components/ui/sidebar";
+
+const user = {
+	name: "shadcn",
+	email: "m@example.com",
+	// TODO: find an avatar image
+	avatar: "/avatars/shadcn.jpg",
+};
+
+export const BedrockAccountMenu = () => {
+	const { disconnect } = useDisconnect();
+	const { logout } = useLogout();
+
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<SidebarMenuButton
+					size="lg"
+					className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+				>
+					<Avatar className="h-8 w-8 rounded-lg">
+						<AvatarImage src={user.avatar} alt={user.name} />
+						<AvatarFallback className="rounded-lg">CN</AvatarFallback>
+					</Avatar>
+					<div className="grid flex-1 text-left text-sm leading-tight">
+						<span className="truncate font-semibold">{user.name}</span>
+						<span className="truncate text-xs">{user.email}</span>
+					</div>
+					<ChevronsUpDown className="ml-auto size-4" />
+				</SidebarMenuButton>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent
+				className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+				side="bottom"
+				align="end"
+				sideOffset={4}
+			>
+				<DropdownMenuLabel className="p-0 font-normal">
+					<div className="flex items-center gap-2 px-1 py-1.5 text-left text-sm">
+						<Avatar className="h-8 w-8 rounded-lg">
+							<AvatarImage src={user.avatar} alt={user.name} />
+							<AvatarFallback className="rounded-lg">CN</AvatarFallback>
+						</Avatar>
+						<div className="grid flex-1 text-left text-sm leading-tight">
+							<span className="truncate font-semibold">{user.name}</span>
+							<span className="truncate text-xs">{user.email}</span>
+						</div>
+					</div>
+				</DropdownMenuLabel>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<Sparkles />
+						Upgrade to Pro
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<BadgeCheck />
+						Account
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<CreditCard />
+						Billing
+					</DropdownMenuItem>
+					<DropdownMenuItem>
+						<Bell />
+						Notifications
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+				<DropdownMenuItem
+					onClick={() => {
+						disconnect();
+						logout();
+					}}
+				>
+					<LogOut />
+					Log out
+				</DropdownMenuItem>
+			</DropdownMenuContent>
+		</DropdownMenu>
+	);
+};
