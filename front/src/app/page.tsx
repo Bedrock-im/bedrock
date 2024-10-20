0920cbf87525cf5ea8dc91ebb37a2893fd8ec8ec
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-10-20 20:27:14 +0900

    feat(front): Sidebar setup

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -1,15 +1,17 @@
 "use client";
-import { usePrivy } from "@privy-io/react-auth";
 import { useAccount } from "wagmi";
 
-import { Button } from "@/components/ui/button";
+import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Home() {
-	const { login } = usePrivy();
 	const account = useAccount();
 	return (
 		<>
-			<Button onClick={login}>Login with Privy</Button>
+			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
+				<div className="flex items-center gap-2 px-4">
+					<SidebarTrigger className="-ml-1" />
+				</div>
+			</header>
 			{account.address ?? "Not connected"}
 		</>
 	);
