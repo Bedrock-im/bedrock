5e7071a53f180e5592b48f6d4f16de3551b70913
Florian Lauch <florian.lauchh@gmail.com>
2024-10-20 19:47:59 +0900

    feat(components): Avatar component

---
+++ front/src/components/ui/avatar.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { Avatar as RadixAvatar, AvatarImage, Fallback } from "@radix-ui/react-avatar";
+import React from "react";
+
+import { cn } from "@/lib/utils";
+
+export type AvatarProps = React.ImgHTMLAttributes<HTMLImageElement>;
+
+const Avatar = React.forwardRef<React.ElementRef<typeof AvatarImage>, AvatarProps>(
+	({ className, src, alt, ...props }, ref) => (
+		<RadixAvatar>
+			<AvatarImage className={cn("rounded-full h-12 w-12", className)} ref={ref} src={src} alt={alt} {...props} />
+			<Fallback
+				className={cn("flex h-12 w-12 items-center justify-center rounded-full bg-muted", className)}
+				ref={ref}
+				{...props}
+			>
+				{alt}
+			</Fallback>
+		</RadixAvatar>
+	),
+);
+Avatar.displayName = "Avatar";
+
+export { Avatar };
