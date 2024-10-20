b84292a4d1317040bee07e4bb950b2f4ab65b044
Florian Lauch <florian.lauchh@gmail.com>
2024-10-20 20:41:18 +0900

    feat(components): Breadcrumb component

--- front/src/components/ui/avatar.tsx
+++ front/src/components/ui/avatar.tsx
@@ -12,7 +12,7 @@ const Avatar = React.forwardRef<React.ElementRef<typeof AvatarImage>, AvatarProp
 		<RadixAvatar>
 			<AvatarImage className={cn("rounded-full h-12 w-12", className)} ref={ref} src={src} alt={alt} {...props} />
 			<Fallback
-				className={cn("flex h-12 w-12 items-center justify-center rounded-full bg-muted", className)}
+				className={cn("flex h-12 w-12 items-center justify-center rounded-full bg-secondary text-primary", className)}
 				ref={ref}
 				{...props}
 			>
