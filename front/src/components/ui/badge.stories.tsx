56eccd22eac2747c6ac0566dd7c46ab8e03cd988
Florian Lauch <florian.lauchh@gmail.com>
2024-10-20 20:12:58 +0900

    feat(components): Badge component

---
+++ front/src/components/ui/badge.stories.tsx
@@ -0,0 +1,64 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Badge, BadgeProps } from "@/components/ui/badge";
+
+const meta: Meta<BadgeProps> = {
+	component: Badge,
+	title: "Badge",
+	args: {
+		size: "default",
+		children: "Badge content",
+		className: "",
+	},
+	argTypes: {
+		variant: {
+			type: "string",
+			control: "select",
+			options: ["default", "outline", "secondary", "destructive"],
+		},
+		size: {
+			type: "string",
+			control: "select",
+			options: ["default", "sm", "lg"],
+		},
+		className: {
+			type: "string",
+			control: "text",
+		},
+	},
+};
+type Story = StoryObj<BadgeProps>;
+
+const Render = (args: Meta<BadgeProps>) => <Badge {...args} />;
+
+export const Default: Story = {
+	args: {
+		variant: "default",
+		size: "default",
+	},
+	parameters: {
+		backgrounds: {
+			default: "light",
+		},
+	},
+	render: Render,
+};
+
+export const DarkMode: Story = {
+	args: {
+		variant: "default",
+		size: "default",
+	},
+	parameters: {
+		backgrounds: {
+			default: "dark",
+		},
+	},
+	render: (args) => (
+		<div className="dark">
+			<Render {...args} />
+		</div>
+	),
+};
+
+export default meta;
