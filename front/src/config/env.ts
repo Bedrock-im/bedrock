3959e67e3700faada3340feaa5a1cfff1d8f547d
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-10-26 03:23:03 +0900

    feat(aleph): Service setup started

--- front/src/config/env.ts
+++ front/src/config/env.ts
@@ -2,8 +2,12 @@ import { z } from "zod";
 
 const envSchema = z.object({
 	PRIVY_APP_ID: z.string(),
+	ALEPH_API_URL: z.string().optional(),
 });
 
-const env = envSchema.parse({ PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID });
+const env = envSchema.parse({
+	PRIVY_APP_ID: process.env.NEXT_PUBLIC_PRIVY_APP_ID,
+	ALEPH_API_URL: process.env.NEXT_PUBLIC_ALEPH_API_URL,
+});
 
 export default env;
