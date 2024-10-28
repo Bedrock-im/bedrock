5363018824044bcf194e7196988cb8d750936bde
Reza Rahemtola <reza.rahemtola@epitech.eu>
2024-10-28 15:06:52 +0900

    feat(account): Add support for ENS names and avatars

--- front/src/app/(drive)/page.tsx
+++ front/src/app/(drive)/page.tsx
@@ -1,10 +1,6 @@
-"use client";
-import { useAccount } from "wagmi";
-
 import { SidebarTrigger } from "@/components/ui/sidebar";
 
 export default function Home() {
-	const account = useAccount();
 	return (
 		<>
 			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
@@ -12,7 +8,6 @@ export default function Home() {
 					<SidebarTrigger className="-ml-1" />
 				</div>
 			</header>
-			{account.address ?? "Not connected"}
 		</>
 	);
 }
