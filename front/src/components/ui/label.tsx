e17e56d14d63d8d464acdf15818fb8cb7a74602f
Florian Lauch <florian.lauchh@gmail.com>
2024-11-03 15:01:00 +0900

    feat(components): Label component and stories

---
+++ front/src/components/ui/label.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { Root } from "@radix-ui/react-label";
+import React from "react"
+
+import { cn } from "@/lib/utils"
+
+export type LabelProps = React.ComponentPropsWithoutRef<typeof Root>
+
+const Label = React.forwardRef<React.ElementRef<typeof Root>, LabelProps>(({ className, ...props }, ref) => (
+  <Root
+    ref={ref}
+    className={cn("text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70 text-primary", className)}
+    {...props}
+  />
+))
+Label.displayName = Root.displayName
+
+export { Label }
