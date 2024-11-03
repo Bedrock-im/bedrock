6cbd05dd0282fa30f4024af96c2bee4da4df9e78
Florian Lauch <florian.lauchh@gmail.com>
2024-11-03 15:01:00 +0900

    feat(components): Label component and stories

---
+++ front/src/components/ui/label.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from "@storybook/react";
+
+import { Checkbox } from "@/components/ui/checkbox";
+import { Label, LabelProps } from "@/components/ui/label";
+
+const meta: Meta<LabelProps> = {
+	component: Label,
+	title: "Label",
+	args: {
+		className: "",
+	},
+	argTypes: {
+		className: {
+			type: "string",
+			control: "text",
+		},
+	},
+};
+type Story = StoryObj<LabelProps>;
+
+const Render = (args: LabelProps) => (
+	<div className="flex items-center space-x-2">
+		<Checkbox id="example" />
+		<Label htmlFor="example" {...args}>Click me!</Label>
+	</div>
+);
+
+export const Default: Story = {
+	args: {
+		className: "",
+	},
+	parameters: {
+		backgrounds: {
+			default: "light",
+		},
+	},
+	render: Render,
+};
+
+export const DarkMode: Story = {
+	args: {
+		className: "",
+	},
+	parameters: {
+		backgrounds: {
+			default: "dark",
+		},
+	},
+	render: (args) => (
+		<div className="dark">
+			<Render {...args} />
+		</div>
+	),
+};
+
+export default meta;
