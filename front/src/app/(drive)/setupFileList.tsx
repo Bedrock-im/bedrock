c326bbcf973b521a65c8fa5fa869ea08a4959a3a
chuipagro <pablo.levy@epitech.eu>
2024-11-05 20:24:24 +0800

    feat: add basic drive interface

--- front/src/app/(drive)/setupFileList.tsx
+++ front/src/app/(drive)/setupFileList.tsx
@@ -1,55 +1,39 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import FileList from "@/components/drive/fileList";
-import { Separator } from "@/components/ui/separator";
-import { Permission } from "@/utils/types";
-import { useAccountStore } from "@/stores/bedrockAccount";
+import { useEffect, useState } from 'react';
+
+import FileList from '@/components/drive/fileList';
+import { Permission } from '@/utils/types';
+
 import "./drive.css";
-
 const SetupFileList = () => {
-	const [files, setFiles] = useState<
-		{ name: string; size: number; id: string; createdAt: string; permission: Permission; path: string }[]
-	>([]);
-	const [folders, setFolders] = useState<{ name: string; permission: Permission; path: string }[]>([]);
-	const [searchQuery, setSearchQuery] = useState<string>("");
+	const [files, setFiles] = useState<{ name: string; size: number; id: string; createdAt: string; permission: Permission, path: string
+	}[]>([]);
+	const [folders, setFolders] = useState<{ name: string; permission: Permission, path: string
+	}[]>([]);
+	const [userPath, setUserPath] = useState<string>('/');
+	const [searchQuery, setSearchQuery] = useState<string>('');
 
-	const bedrockService = useAccountStore((state) => state.bedrockService);
 
 	useEffect(() => {
 		const fetchData = async () => {
-			if (!bedrockService) {
-				console.log("BedrockService n'est pas disponible.");
-				return;
-			}
-
-			try {
-				console.log(bedrockService)
-				const fileEntries = await bedrockService.fetchFileEntries();
-				console.log("files entries", fileEntries)
-				const formattedFiles = fileEntries.map((entry) => ({
-					name: entry.path.split("/").pop() || "Unnamed file",
-					size: Math.floor(Math.random() * 500) + 100,
-					id: entry.post_hash,
-					createdAt: new Date().toISOString().split("T")[0],
-					permission: "viewer" as Permission,
-					path: entry.path,
-				}));
-
-				setFiles(formattedFiles);
-			} catch (error) {
-				console.error("Erreur lors de la récupération des fichiers :", error);
-			}
 		};
 
 		fetchData();
-	}, [bedrockService]);
+	}, []);
 
-	const filteredFiles = files.filter((file) => file.name.toLowerCase().includes(searchQuery.toLowerCase()));
-	const filteredFolders = folders.filter((folder) => folder.name.toLowerCase().includes(searchQuery.toLowerCase()));
+	const filteredFiles = files.filter((file) =>
+		file.name.toLowerCase().includes(searchQuery.toLowerCase())
+	);
+	const filteredFolders = folders.filter((folder) =>
+		folder.name.toLowerCase().includes(searchQuery.toLowerCase())
+	);
 
 	return (
 		<div className="drive-container">
+			<div className="drive-title">
+				<h1>My Decentralized Drive</h1>
+			</div>
 			<div className="search-bar mb-4">
 				<input
 					type="text"
@@ -60,13 +44,8 @@ const SetupFileList = () => {
 				/>
 			</div>
 			<div className="drive-content">
-				{bedrockService ? (
-					<FileList files={filteredFiles} folders={filteredFolders} bedrockService={bedrockService} />
-				) : (
-					<p>Le service Bedrock nest pas disponible. Veuillez vous connecter.</p>
-				)}
+				<FileList files={filteredFiles} folders={filteredFolders} />
 			</div>
-			<Separator />
 		</div>
 	);
 };
