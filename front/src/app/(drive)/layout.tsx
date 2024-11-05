4ce3280327e99107b57107f89698191bc3e13ed8
chuipagro <pablo.levy@epitech.eu>
2024-11-05 20:29:06 +0800

    feat: fix css within fileList component

--- front/src/app/(drive)/layout.tsx
+++ front/src/app/(drive)/layout.tsx
@@ -2,6 +2,7 @@ import { ReactNode } from "react";
 
 import { AuthWrapper } from "@/components/AuthWrapper";
 import { BedrockSidebar } from "@/components/BedrockSidebar";
+
 import SetupFileList from "./setupFileList";
 
 export default function DriveLayout({ children }: Readonly<{ children: ReactNode }>) {
