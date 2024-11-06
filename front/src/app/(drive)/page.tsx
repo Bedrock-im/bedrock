640b4e510330b80c5e1e7f8bb336940b28cb68c2
Croos3r <dorian.moy@epitech.eu>
2024-11-06 15:12:12 +0900

    fix(file-upload): last bugs
    Took 5 hours 17 minutes

--- front/src/app/(drive)/page.tsx
+++ front/src/app/(drive)/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+import { useCallback } from "react"; /*TODO: REMOVE TESTING AFTER REVIEW*/
+
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import useBedrockFileUploadDropzone from "@/hooks/useBedrockFileUploadDropzone";
 import { useBedrockAccountStore } from "@/stores/bedrockAccount";
@@ -11,6 +13,12 @@ export default function Home() {
 		bedrockService: bedrockService!,
 	});
 
+	const onFetch = useCallback(async () => {
+		/*TODO: REMOVE TESTING AFTER REVIEW*/
+		if (bedrockService === null) return;
+		console.log("fileEntries", await bedrockService!.fetchFileEntries());
+	}, [bedrockService]);
+
 	return (
 		<section className="h-full">
 			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
@@ -18,7 +26,9 @@ export default function Home() {
 					<SidebarTrigger className="-ml-1" />
 				</div>
 			</header>
+			<button onClick={onFetch}>Log fetched file entries</button> {/*TODO: REMOVE TESTING AFTER REVIEW*/}
 			<div className="h-full" {...getRootProps()}>
+				{/*TODO: REMOVE TESTING AFTER REVIEW*/}
 				<input {...getInputProps()} />
 				<p>Upload files here</p>
 			</div>
