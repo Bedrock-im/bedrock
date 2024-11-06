07e30e704d0e1dae10feb6301b0c5c7c5f00e1dd
chuipagro <pablo.levy@epitech.eu>
2024-11-06 21:27:47 +0800

    feat(fileList): add basic file selection

---
+++ front/src/components/drive/drivePageTitle.tsx
@@ -0,0 +1,26 @@
+"use client"
+
+import React from 'react';
+
+import "@/app/(drive)/drive.css";
+
+
+interface DrivePageTitleProps {
+	selectedItemsCount: number;
+}
+
+export const DrivePageTitle: React.FC<DrivePageTitleProps> = ({ selectedItemsCount }) => {
+	return (
+		<div>
+			{selectedItemsCount === 0 ? (
+				<div className="drive-title">
+							<h1>My Drive</h1>
+				</div>
+			) : (
+				<div className="drive-title">
+					<h1>{`${selectedItemsCount} fichier(s) sélectionné(s)`}</h1>
+				</div>
+			)}
+		</div>
+	);
+};
