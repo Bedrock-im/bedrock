ad4ef1485f4351d614619747adb7790a721b8811
Croos3r <dorian.moy@epitech.eu>
2024-11-06 19:41:40 +0900

    fix(file-upload): removed test components in drive page
    Took 5 minutes

--- front/src/app/(drive)/page.tsx
+++ front/src/app/(drive)/page.tsx
@@ -1,24 +1,8 @@
 "use client";
 
-import { useCallback } from "react"; /*TODO: REMOVE TESTING AFTER REVIEW*/
-
 import { SidebarTrigger } from "@/components/ui/sidebar";
-import useBedrockFileUploadDropzone from "@/hooks/useBedrockFileUploadDropzone";
-import { useAccountStore } from "@/stores/bedrockAccount";
 
 export default function Home() {
-	const { bedrockService } = useAccountStore();
-	const { getInputProps, getRootProps } = useBedrockFileUploadDropzone({
-		current_directory: "/",
-		bedrockService: bedrockService!,
-	});
-
-	const onFetch = useCallback(async () => {
-		/*TODO: REMOVE TESTING AFTER REVIEW*/
-		if (bedrockService === null) return;
-		console.log("fileEntries", await bedrockService!.fetchFileEntries());
-	}, [bedrockService]);
-
 	return (
 		<section className="h-full">
 			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
@@ -26,12 +10,6 @@ export default function Home() {
 					<SidebarTrigger className="-ml-1" />
 				</div>
 			</header>
-			<button onClick={onFetch}>Log fetched file entries</button> {/*TODO: REMOVE TESTING AFTER REVIEW*/}
-			<div className="h-full" {...getRootProps()}>
-				{/*TODO: REMOVE TESTING AFTER REVIEW*/}
-				<input {...getInputProps()} />
-				<p>Upload files here</p>
-			</div>
 		</section>
 	);
 }
