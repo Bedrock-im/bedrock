f9c4ffb832e83ff1f997b7621b1fe502e38ed57e
Reza Rahemtola <reza.rahemtola@epitech.eu>
2025-03-17 17:21:56 +0900

    ci(front): Deploy previews on Aleph

--- front/next.config.mjs
+++ front/next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import("next").NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+	output: "export",
+	trailingSlash: true,
+	images: { unoptimized: true },
+};
 
 export default nextConfig;
