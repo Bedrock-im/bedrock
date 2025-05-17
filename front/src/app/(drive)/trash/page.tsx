278d013a2e61c20d1396f9ac5de8eaaa3c735161
Florian Lauch <florian.lauchh@gmail.com>
2025-05-17 09:34:22 +0900

    feat: Sharing warm-up

--- front/src/app/(drive)/trash/page.tsx
+++ front/src/app/(drive)/trash/page.tsx
@@ -3,7 +3,6 @@
 import { useQueryState } from "nuqs";
 
 import FileList from "@/components/drive/FileList";
-import { SidebarTrigger } from "@/components/ui/sidebar";
 import { useDriveStore } from "@/stores/drive";
 
 export default function Trash() {
@@ -28,11 +27,6 @@ export default function Trash() {
 	);
 	return (
 		<section>
-			<header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12">
-				<div className="flex items-center gap-2 px-4">
-					<SidebarTrigger className="-ml-1" />
-				</div>
-			</header>
 			<FileList
 				files={filteredFiles}
 				folders={filteredFolders}
