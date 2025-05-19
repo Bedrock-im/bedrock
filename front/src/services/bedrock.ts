4c060230d53e2c22b4376188598e93913d35e2d6
Croos3r <dorian.moy@epitech.eu>
2025-05-20 00:54:34 +0900

    fix(front-knowledge-base): wrong named passed to sent knowledge bases after modification in the add and remove file method
    Took 5 minutes
    
    
    Took 15 seconds

--- front/src/services/bedrock.ts
+++ front/src/services/bedrock.ts
@@ -603,14 +603,14 @@ export default class BedrockService {
 					const decryptedName = EncryptionService.decryptEcies(kbName, this.alephService.encryptionPrivateKey.secret);
 					if (decryptedName !== name) {
 						return {
-							name,
+							name: kbName,
 							file_paths,
 							updated_at,
 							...rest,
 						};
 					}
 					return {
-						name,
+						name: kbName,
 						file_paths: file_paths.filter(
 							(kbFilePath) =>
 								EncryptionService.decryptEcies(
@@ -638,14 +638,14 @@ export default class BedrockService {
 					const decryptedName = EncryptionService.decryptEcies(kbName, this.alephService.encryptionPrivateKey.secret);
 					if (decryptedName !== name) {
 						return {
-							name,
+							name: kbName,
 							file_paths,
 							updated_at,
 							...rest,
 						};
 					}
 					return {
-						name,
+						name: kbName,
 						file_paths: [
 							...file_paths,
 							EncryptionService.encryptEcies(filePath, this.alephService.encryptionPrivateKey.publicKey.compressed),
