170b24ffdd694e78c8c7f9546128b0744243518d
Florian Lauch <florian.lauchh@gmail.com>
2025-07-15 20:34:45 +0200

    fix: additional toasts

--- front/src/components/drive/FileList.tsx
+++ front/src/components/drive/FileList.tsx
@@ -244,6 +244,7 @@ const FileList: React.FC<FileListProps> = ({
 			}
 			bedrockService?.hardDeleteFiles(fileToDelete);
 		}
+		toast.success(`The ${folder ? "folder" : "file"} has been permanently deleted.`);
 	};
 
 	const handleMove = (path: string, newPath: string, folder: boolean) => {
@@ -254,6 +255,7 @@ const FileList: React.FC<FileListProps> = ({
 			const filesToMove = moveFolder(path, newPath);
 			filesToMove.map(([oldFile, newFile]) => bedrockService?.moveFile(oldFile.path, newFile.path));
 		}
+		toast.success(`The ${folder ? "folder" : "file"} has been moved.`);
 	};
 
 	const handleShare = (file: FileFullInfos, contact: Contact) => {
